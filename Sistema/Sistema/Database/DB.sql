7f39ca60c5f643e5b7a673c84f37cbdb2fefc3df
Felipe Corrêa <105026938+FelipeECorrea@users.noreply.github.com>
2022-07-25 20:21:05 -0300

    Atualização DB

---
+++ Sistema/Sistema/Database/DB.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE unidades_federativas (
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	sigla VARCHAR(2)
+);
+
+CREATE TABLE cidades (
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+
+	id_unidade_federativa INTEGER, -- FK
+
+	nome VARCHAR(50),
+	populacao INTEGER,
+	data_criacao DATETIME2,
+	pib DECIMAL(6,2),
+
+	FOREIGN KEY(id_unidade_federativa) REFERENCES unidades_federativas(id)
+);
+
+
+SELECT * FROM unidades_federativas;
+SELECT * FROM cidades;
+
+
+INSERT INTO cidades (id_unidade_federativa, nome, populacao, data_criacao, pib) VALUES
+(2,'Blumenau',21,'2022-07-10',1);
