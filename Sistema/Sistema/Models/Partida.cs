dc1ccbe59e00f48ac676d2db404e08bcf96e0540
Kauã Amaral <kauaamaral10@gmail.com>
2022-07-25 22:55:08 -0300

    Model Partida Adicionado

--- Sistema/Sistema/Models/Partida.cs
+++ Sistema/Sistema/Models/Partida.cs
@@ -2,5 +2,17 @@
 {
     internal class Partida
     {
+        public int Id { get; set; }
+        public string GrupoSorteado { get; set; }
+        public bool MapaMirage { get; set; }
+        public bool MapaDust2 { get; set; }
+        public bool MapaOverpass { get; set; }
+        public bool MapaVertigo { get; set; }
+        public bool MapaNuke { get; set; }
+        public bool MapaInferno { get; set; }
+        public bool MapaTrain { get; set; }
+
+        public Time Time { get; set; }
+        public Torneio Torneio { get; set; }
     }
 }
