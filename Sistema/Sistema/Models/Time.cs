4e01258eaf8d9578dac3b6fa43ca13563a5f258c
Kauã Amaral <kauaamaral10@gmail.com>
2022-07-27 19:33:10 -0300

    Model Time Atualizado atualizado models do time

--- Sistema/Sistema/Models/Time.cs
+++ Sistema/Sistema/Models/Time.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sistema.Models
 {
     internal class Time
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Nacionalidade { get; set; }
+        public DateTime DataCriacao { get; set; }
     }
 }
