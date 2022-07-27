bb465b8aa62b4768845da7e0e0edb9bfe90a2fac
Kauã Amaral <kauaamaral10@gmail.com>
2022-07-27 20:04:52 -0300

    model da transmissao atualizada

--- Sistema/Sistema/Models/Transmissao.cs
+++ Sistema/Sistema/Models/Transmissao.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sistema.Models
+﻿namespace Sistema.Models
 {
     internal class Transmissao
     {
+		public int Id { get; set; }
+		public string NomeLive { get; set; }
+		public string NomeNarrador { get; set; }
+		public string Plataforma { get; set; }
+		public bool IdiomaPortugues { get; set; }
+		public bool IdiomaIngles { get; set; }
+		public bool IdiomaEspanhol { get; set; }
+
+		public Torneio Torneio { get; set; }
     }
 }
