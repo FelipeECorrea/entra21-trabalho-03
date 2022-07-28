56c398df648a548096363336e475abc368ed8e64
Kauã Amaral <kauaamaral10@gmail.com>
2022-07-27 21:23:24 -0300

    Service Atualizado

---
+++ Sistema/Sistema/Service/ITorneioService.cs
@@ -0,0 +1,10 @@
+﻿using Sistema.Models;
+using System.Collections.Generic;
+
+namespace Sistema.Service
+{
+    internal interface ITorneioService
+    {
+        List<Torneio> ObterTodos();
+    }
+}
