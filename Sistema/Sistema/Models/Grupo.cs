056dbf415332bb8ee4dcd8f597c5e477193dea37
Kauã Amaral <kauaamaral10@gmail.com>
2022-08-02 18:26:28 -0300

    Models arrumado / Service arrumado

--- Sistema/Sistema/Models/Grupo.cs
+++ Sistema/Sistema/Models/Grupo.cs
@@ -6,9 +6,5 @@
 
         public Time Time { get; set; }
         public Torneio Torneio { get; set; }
-<<<<<<< HEAD
-=======
-        public Transmissao Transmissao { get; set; }
->>>>>>> fb7dd9544338200617a94bdb25a60b4fe58c77ee
     }
 }
